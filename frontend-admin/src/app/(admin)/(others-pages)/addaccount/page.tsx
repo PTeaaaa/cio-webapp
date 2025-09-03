e7ab2ce7076e3c81ebfb76f95c327926fca10d50
ptea <testtest.wahoi@gmail.com>
2025-09-03 16:50:22 +0700

    add left untracked file

---
+++ frontend-admin/src/app/(admin)/(others-pages)/addaccount/page.tsx
@@ -0,0 +1,23 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import AddNewAccount from "@/components/auth/AddNewAccount";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "CIOSite - Edit",
+  description:
+    "This is CIOSite editing page.",
+};
+
+export default function EditPage() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="เพิ่มข้อมูลใหม่" />
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+        <div className="space-y-6">
+          <AddNewAccount />
+        </div>
+      </div>
+    </div>
+  );
+}
